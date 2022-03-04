b9b1e4049fe7c03099409580584ce3b941d04d41
Umbreller-F <umbreller_f@163.com>
2022-03-04 09:25:57 +0800

    exp2框架

---
+++ exp2.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import re
+import urllib.request,urllib.error
+import xlwt
+
+def fun1():
+    base_url = "https://movie.douban.com/top250?start="
+    save_path = ""
+    return 0
+
+def getData(base_url):
+    datalist = []
+    return datalist
+
+def saveData(path):
+    pass
+
+if __name__ =="__main__":
+    fun1()
